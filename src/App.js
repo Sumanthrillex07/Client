e5d6987b274ae87bfb1cfd5515656428627e01b8
Sumanthrillex07 <p.21_ug@cse.nits.ac.in>
2024-02-15 08:20:27 +0530

    added scroll

--- src/App.js
+++ src/App.js
@@ -7,31 +7,38 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [bool, setBool] = useState(false);
 
   const joinRoom = () => {
     if (name !== "" && room !== "") {
       socket.emit("join", room);
+      setBool(true);
     }
   };
   return (
     <div className="App">
-      <h3>Join Chat</h3>
-      <input
-        type="text"
-        placeholder="Name"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Room ID"
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>Start Chat</button>
-      <Chat socket={socket} name={name} room={room} />
+      {!bool ? (
+        <div className="chatContainer">
+          <h3>Join Chat</h3>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Start Chat</button>
+        </div>
+      ) : (
+        <Chat socket={socket} name={name} room={room} />
+      )}
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 
 const Chat = ({ socket, name, room }) => {
   const [msg, setMsg] = useState("");
@@ -15,12 +16,14 @@ const Chat = ({ socket, name, room }) => {
           new Date(Date.now()).getMinutes(),
       };
       await socket.emit("send", msgData);
+      setMsgList((list) => [...list, msgData]);
+      setMsg("");
     }
   };
   useEffect(() => {
     socket.on("receive", async (data) => {
       //   console.log(data);
-     await setMsgList((list) => [...list, data]);
+      await setMsgList((list) => [...list, data]);
     });
     return () => socket.off("receive");
   }, [socket]);
@@ -30,16 +33,37 @@ const Chat = ({ socket, name, room }) => {
         <p>Live chat</p>
       </div>
       <div className="chat-body">
-        {msgList.map((msgCt) => {
-          return <div>{msgCt.msg}</div>;
-        })}
+        <ScrollToBottom className="message-container">
+          {msgList.map((msgCt) => {
+            return (
+              <div
+                className="message"
+                id={name === msgCt.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="msg-cnt">
+                    <p>{msgCt.msg}</p>
+                  </div>
+                  <div className="msg-meta">
+                    <p id="time">{msgCt.time}</p>
+                    <p id="author">{msgCt.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
       </div>
       <div className="chat-footer">
         <input
           type="text"
+          value={msg}
           placeholder="write.."
           onChange={(event) => {
             setMsg(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMsg();
           }}
         />
         <button onClick={sendMsg}>&#9658;</button>
