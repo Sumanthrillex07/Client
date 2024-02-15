cdda8e0b76c9913bc96d359b7528dd8f6b88f367
Sumanthrillex07 <p.21_ug@cse.nits.ac.in>
2024-02-15 07:59:26 +0530

    added little css

--- src/Chat.js
+++ src/Chat.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Chat = ({ socket, name, room }) => {
   const [msg, setMsg] = useState("");
-  const sendMsg = () => {
+  const [msgList, setMsgList] = useState([]);
+  const sendMsg = async () => {
     if (msg !== "") {
       const msgData = {
         room: room,
@@ -13,14 +14,26 @@ const Chat = ({ socket, name, room }) => {
           ":" +
           new Date(Date.now()).getMinutes(),
       };
+      await socket.emit("send", msgData);
     }
   };
+  useEffect(() => {
+    socket.on("receive", async (data) => {
+      //   console.log(data);
+     await setMsgList((list) => [...list, data]);
+    });
+    return () => socket.off("receive");
+  }, [socket]);
   return (
-    <div>
+    <div className="chat-window">
       <div className="chat-header">
         <p>Live chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        {msgList.map((msgCt) => {
+          return <div>{msgCt.msg}</div>;
+        })}
+      </div>
       <div className="chat-footer">
         <input
           type="text"
@@ -29,7 +42,7 @@ const Chat = ({ socket, name, room }) => {
             setMsg(event.target.value);
           }}
         />
-        <button>&#9658;</button>
+        <button onClick={sendMsg}>&#9658;</button>
       </div>
     </div>
   );
