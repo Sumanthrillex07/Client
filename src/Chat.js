eaf4b10d56c100537ef5d2a0266d6d0477fae7a8
Sumanthrillex07 <p.21_ug@cse.nits.ac.in>
2024-02-15 12:33:33 +0530

    css added

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
+import onlineIcon from "./icons/closeIcon.png";
+
+let room;
 
 const Chat = ({ socket, name, room }) => {
   const [msg, setMsg] = useState("");
   const [msgList, setMsgList] = useState([]);
+  const [users, setUsers] = useState([]);
   const sendMsg = async () => {
     if (msg !== "") {
       const msgData = {
@@ -15,8 +19,10 @@ const Chat = ({ socket, name, room }) => {
           ":" +
           new Date(Date.now()).getMinutes(),
       };
+      room = msgData.room;
       await socket.emit("send", msgData);
       setMsgList((list) => [...list, msgData]);
+      
       setMsg("");
     }
   };
@@ -24,13 +30,17 @@ const Chat = ({ socket, name, room }) => {
     socket.on("receive", async (data) => {
       //   console.log(data);
       await setMsgList((list) => [...list, data]);
+    //   await setUsers((list) => [...list, data.author]);
     });
     return () => socket.off("receive");
   }, [socket]);
   return (
     <div className="chat-window">
       <div className="chat-header">
-        <p>Live chat</p>
+        <p>ROOM: {room}</p>
+        <a href="/">
+          X
+        </a>
       </div>
       <div className="chat-body">
         <ScrollToBottom className="message-container">
